5431acf586508a83a45031e397da025e459d5b9a
slavamel <wait4answer@gmail.com>
2019-11-27 16:19:54 +0300

    [front] add country holiday table with adding and removing buttons

--- src-frontend/src/app/components/country-holidays/country-holidays.component.ts
+++ src-frontend/src/app/components/country-holidays/country-holidays.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HolidayServiceMock } from 'src/app/services/holiday/holiday.service.mock';
+import { Holiday } from 'src/app/models/holiday.model';
 
 @Component({
   selector: 'app-country-holidays',
@@ -6,8 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./country-holidays.component.css']
 })
 export class CountryHolidaysComponent implements OnInit {
+  year = 2019;
+  holidays: Holiday[];
+  startDate: Date;
+  endDate: Date;
 
-  constructor() { }
+  isLoading: boolean;
 
-  ngOnInit() { }
+  constructor(private holidayService: HolidayServiceMock) { }
+
+  ngOnInit() { 
+    this.initCountryHolidays();
+  }
+
+  onStartDateChanged(date: string): void {
+    this.startDate = new Date(date);
+  }
+
+  onEndDateChanged(date: string): void {
+    this.endDate = new Date(date);
+  }
+
+  onAddHolidayClicked(): void {
+    this.holidays.push(new Holiday(this.startDate, this.endDate));
+    this.startDate = null;
+    this.endDate = null;
+  }
+
+  onRemoveHolidayClicked(index): void {
+    this.holidays.splice(index, 1);
+  }
+
+  private initCountryHolidays(): void {
+    this.holidayService.getCountryHolidays(this.year)
+      .then(holidays => this.holidays = holidays)
+      .finally(() => this.isLoading = false);
+  }
 }
