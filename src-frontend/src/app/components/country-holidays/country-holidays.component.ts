8c44d1f14c88b075e6f85b8d145301dfac85f5ba
slavamel <wait4answer@gmail.com>
2019-11-29 02:14:12 +0300

    [front] add checking for adding existing holiday

--- src-frontend/src/app/components/country-holidays/country-holidays.component.ts
+++ src-frontend/src/app/components/country-holidays/country-holidays.component.ts
@@ -60,13 +60,20 @@ export class CountryHolidaysComponent implements OnInit {
     if (!this.startDate || !this.endDate) {
       this.error = "Add dates first";
       return false;
-    };
+    }
+
     if (this.startDate > this.endDate) {
       this.error = "Swap the dates";
       return false;
-    };
+    }
+
     if (this.startDate.getFullYear() != this.year || this.endDate.getFullYear() != this.year) {
       this.error = `Change calendar's year first`;
+      return false;
+    }
+
+    if (this.isDateAlreadyExist()) {
+      this.error = `The dates already exist in another holiday`;
       return false;
     }
 
@@ -77,5 +84,29 @@ export class CountryHolidaysComponent implements OnInit {
   private initCountryHolidays(): void {
     this.holidayService.getCountryHolidays(this.year)
       .then(holidays => this.holidays = holidays);
+  }
+
+  private isDateAlreadyExist(): boolean {
+    let holidaysTimes = [];
+    this.holidays.forEach(holiday => {
+      holidaysTimes.push(...this.getAllDateTimes(holiday));
+    });
+
+    let newHolidayTimes = this.getAllDateTimes(new Holiday(this.startDate, this.endDate));
+    
+    const result = holidaysTimes.some(h => newHolidayTimes.includes(h));
+    return result;
+  }
+
+  private getAllDateTimes(holiday: Holiday): number[] {
+    let times = [];
+    let currentDate = new Date(holiday.startDate);
+    while (true) {
+      times.push(currentDate.getTime());
+      if (currentDate.getTime() == holiday.endDate.getTime()) break;
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    return times;
   }
 }
